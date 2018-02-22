86ae8ef6a1174ef158623227b848cb9ddc931867
Matt <osiris@protonmail.com>
2018-02-22 23:11:07 +0000

    JQuery Smooth Scrolling

---
+++ main.js
@@ -0,0 +1,13 @@
+// CLICK SMOOTH SCROLL
+
+$(document).ready(function() {
+
+  var scrollLink = $('.scroll');
+
+  // Smooth scrolling
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000 );
+  });
