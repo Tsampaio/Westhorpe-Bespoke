58c03f174363a4d3616fe855a5334137a22f57e4
Matt <osiris@protonmail.com>
2018-02-24 17:27:00 +0000

    Inspiration layout & font updates

--- main.js
+++ main.js
@@ -2,12 +2,26 @@
 
 $(document).ready(function() {
 
-  var scrollLink = $('.scroll');
+	// var scrollLink = $('.scroll');
+	// // Smooth scrolling
+	// scrollLink.click(function(e) {
+	//   e.preventDefault();
+	//   $('body,html').animate({
+	//     scrollTop: $(this.hash).offset().top
+	//   }, 1000 );
+	// });
 
-  // Smooth scrolling
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000 );
-  });
+
+
+  	//Click Smooth Scroll Telmo Code
+  	$(document).on('click', 'a[href^="#"]', function (event) {
+
+		event.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: $($.attr(this, 'href')).offset().top
+		}, 1000);
+
+	});
+
+});
